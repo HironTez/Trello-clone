4ea1db4e10e0a6bb882911e4c2936e98eff33387
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2021-06-26 13:47:39 +0200

    init: start task 8

---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,29 @@
+import * as jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user: { userId: string, login: string };
+    }
+}
+
+const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (token) {
+        // @ts-ignore
+        jwt.verify(token, process.env['JWT_SECRET_KEY']!, (err: Error, user: { userId: string, login: string }) => {
+            if (!err) {
+                req.user = user as { userId: string, login: string };
+            } else {
+                res.sendStatus(403);
+            };
+        });
+    } else {
+        res.sendStatus(401);
+    };
+
+    next();
+};
+
+export default authenticateToken;
